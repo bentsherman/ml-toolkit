e844de4a2da54b063495b1fd39ab4af4dc1b21bd
Ben Shealy <bentshermann@gmail.com>
2018-05-17 08:19:32 -0400

    Refactored extract-sid.py to include labels in output file

--- extract-sid.py
+++ extract-sid.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
 	# parse command-line arguments
 	if len(sys.argv) != 4:
-		print "usage: python extract-gromacs-data.py [dir] [outfile] [num-samples]"
+		print "usage: python extract-sid.py [dir] [outfile] [num-samples]"
 		sys.exit(1)
 
 	INPUT_DIR = sys.argv[1]
@@ -18,16 +18,15 @@ if __name__ == "__main__":
 	dirs = ["%s/%s" % (INPUT_DIR, dir) for dir in os.listdir(INPUT_DIR)]
 	dirs = ["%s/training-data" % (dir) for dir in dirs if os.path.isdir(dir)]
 
-	# initialize data matrix
-	num_features = None
-	data = None
-	columns = []
-
-	# initialize labels
-	labels = []
+	# HACK: reduce sample size to multiple of directories
+	NUM_SAMPLES = NUM_SAMPLES / len(dirs) * len(dirs)
 
 	# iterate through each subdirectory
+	num_features = 0
+	X = None
+	y = None
 	count = 0
+
 	for dir in dirs:
 		print dir
 
@@ -39,31 +38,26 @@ if __name__ == "__main__":
 			# read sample file
 			df = pd.read_csv("%s/%s" % (dir, f), sep="\t", header=None)
 
-			# allocate data matrix on first sample
-			if len(columns) == 0:
+			# allocate X and y on first sample
+			if num_features == 0:
 				num_features = len(df)
-				data = np.empty((NUM_SAMPLES, num_features), dtype=np.float32)
-				columns = df[2].values
+				X = pd.DataFrame(np.empty((NUM_SAMPLES, num_features), dtype=np.float32), None, df[2].values)
+				y = pd.DataFrame(np.empty((NUM_SAMPLES, 1), dtype=object), None, ["structure"])
 
 			# HACK: some sample files have duplicate rows
 			elif len(df) == num_features * 2:
 				df = df.iloc[::2]
 
 			# make sure features are ordered the same
-			elif (columns != df[2].values).all():
+			elif (X.columns != df[2].values).all():
 				print "error: mismatched features"
 				sys.exit(1)
 
-			# append sample to data matrix
-			data[count] = df[4]
+			# append sample
+			X.values[count] = df[4]
+			y.values[count] = label
 			count += 1
 
-		# append labels
-		labels += [label for f in files]
-
 	# save data matrix
-	df = pd.DataFrame(data, None, columns)
+	df = X.join(y)
 	df.to_csv(OUTFILE, sep="\t")
-
-	# TODO: save labels
-	# print labels
