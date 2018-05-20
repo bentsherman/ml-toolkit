9732056411f8259e550245ec5d9d8995cbd42925
Ben Shealy <bentshermann@gmail.com>
2018-05-19 18:16:59 -0700

    Worked on visualize.py

--- visualize.py
+++ visualize.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d
 import numpy as np
 import pandas as pd
-import scipy.stats
 import seaborn as sns
 import sklearn.manifold
 import sklearn.preprocessing
@@ -33,7 +32,6 @@ def transition_matrix(x, y, data):
 
 
 def plot_heatmap(df, X, y):
-	print "Plotting heatmap..."
 	sns.heatmap(X)
 	rotate_xticklabels(45)
 	plt.show()
@@ -41,7 +39,6 @@ def plot_heatmap(df, X, y):
 
 
 def plot_dist_input(df, X, y):
-	print "Plotting distributions of features..."
 	sns.violinplot(data=X, bw=0.2, cut=1, linewidth=1)
 	rotate_xticklabels(45)
 	plt.show()
@@ -49,14 +46,16 @@ def plot_dist_input(df, X, y):
 
 
 def plot_dist_output(df, X, y):
-	print "Plotting distribution of output..."
-	sns.distplot(y)
+	if y.dtype == "object":
+		sns.countplot(y)
+	else:
+		sns.distplot(y)
+
 	plt.show()
 
 
 
 def plot_transitions(df, X, y):
-	print "Plotting transition matrices..."
 	for i in xrange(0, len(config["categorical"]), 2):
 		x = config["categorical"][i]
 		y = config["categorical"][i + 1]
@@ -72,7 +71,6 @@ def plot_transitions(df, X, y):
 
 
 def plot_correlation_heatmap(df, X, y):
-	print "Plotting correlation heatmap..."
 	corr = X.corr()
 	sns.heatmap(corr)
 	rotate_xticklabels(45)
@@ -81,7 +79,6 @@ def plot_correlation_heatmap(df, X, y):
 
 
 def plot_correlation_clustermap(df, X, y):
-	print "Plotting correlation clustermap..."
 	corr = X.corr()
 	sns.clustermap(corr)
 	rotate_xticklabels(45)
@@ -90,7 +87,6 @@ def plot_correlation_clustermap(df, X, y):
 
 
 def plot_pairwise(df, X, y):
-	print "Plotting pairwise distributions..."
 	g = sns.PairGrid(X, diag_sharey=False)
 	g.map_lower(plt.scatter, s=2)
 	g.map_diag(sns.kdeplot, lw=2, legend=False)
@@ -98,37 +94,24 @@ def plot_pairwise(df, X, y):
 
 
 
-def plot_2d(df, X, y):
-	print "Plotting 2-D..."
-	plt.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y, s=2)
-	plt.show()
-
-
-
-def plot_3d(df, X, y):
-	print "Plotting 3-D..."
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection="3d")
-	ax.scatter(X.iloc[:, 0], X.iloc[:, 1], X.iloc[:, 2], c=y, s=2)
-	plt.show()
-
-
-
 def plot_tsne_2d(df, X, y):
-	print "Plotting 2-D t-SNE..."
-	X_tsne = sklearn.manifold.TSNE(n_components=2).fit_transform(X).T
-	plt.scatter(X_tsne[0], X_tsne[1], c=y, s=2)
+	classes = list(set(y))
+	y_cate = [classes.index(y_i) for y_i in y]
+
+	X_tsne = sklearn.manifold.TSNE(n_components=2).fit_transform(X)
+	plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=y_cate, s=2)
 	plt.show()
 
 
 
 def plot_tsne_3d(df, X, y):
-	print "Plotting 3-D t-SNE..."
-	X_tsne = sklearn.manifold.TSNE(n_components=3).fit_transform(X).T
-	density = scipy.stats.gaussian_kde(X_tsne)(X_tsne)
+	classes = list(set(y))
+	y_cate = [classes.index(y_i) for y_i in y]
+
+	X_tsne = sklearn.manifold.TSNE(n_components=3).fit_transform(X)
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection="3d")
-	ax.scatter(X_tsne[0], X_tsne[1], X_tsne[2], c=density, s=2)
+	ax.scatter(X_tsne[:, 0], X_tsne[:, 1], X_tsne[:, 2], c=y_cate, s=2)
 	plt.show()
 
 
@@ -149,23 +132,25 @@ if __name__ == "__main__":
 	X = pd.DataFrame(sklearn.preprocessing.scale(X), X.index, X.columns)
 
 	# remove samples with high-variance output
-	mask = df[config["output"][1]] < 1.5
-	X = X[mask]
-	y = y[mask]
+	output_sd = "%s_SD" % config["output"][0]
+
+	if output_sd in df.columns:
+		mask = df[output_sd] < 1.5
+		X = X[mask]
+		y = y[mask]
 
 	methods = [
-		plot_heatmap,
-		plot_dist_input,
-		plot_dist_output,
-		plot_transitions,
-		plot_correlation_heatmap,
-		plot_correlation_clustermap,
-		plot_pairwise,
-		plot_2d,
-		plot_3d,
-		plot_tsne_2d,
-		plot_tsne_3d
+		("heatmap", plot_heatmap),
+		("feature distributions", plot_dist_input),
+		("output distribution", plot_dist_output),
+		("transition matrices", plot_transitions),
+		("correlation heatmap", plot_correlation_heatmap),
+		("correlation clustermap", plot_correlation_clustermap),
+		("pairwise feature distributions", plot_pairwise),
+		("2-D t-SNE embedding", plot_tsne_2d),
+		("3-D t-SNE embedding", plot_tsne_3d)
 	]
 
-	for method in methods:
+	for (name, method) in methods:
+		print "Plotting %s..." % (name)
 		method(df, X, y)
