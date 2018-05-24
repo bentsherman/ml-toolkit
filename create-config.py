df705fbf46b2b55aeaeec22f17db75b982b9368d
Ben Shealy <bentshermann@gmail.com>
2018-05-23 21:04:21 -0400

    Added initial create-config.py

---
+++ create-config.py
@@ -0,0 +1,32 @@
+import json
+import pandas as pd
+import sys
+
+
+
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print "usage: python generate-config.py [infile] [outfile]"
+		sys.exit(1)
+
+	# load data frame
+	df = pd.read_csv(sys.argv[1], sep="\t")
+
+	# initialize config object
+	config = {
+		"numerical": [],
+		"categorical": [],
+		"output": []
+	}
+
+	# add data frame columns to config
+	for column in df.columns:
+		if df[column].dtype == "object":
+			config["categorical"].append(column)
+		else:
+			config["numerical"].append(column)
+
+	# save config file
+	outfile = open(sys.argv[2], "w")
+	json.dump(config, outfile, indent=2)
+	outfile.close()
