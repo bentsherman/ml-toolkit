0171c850db3107b155f3f417f504b046394d3bbf
eceftl2 <eceftl2@eceftl2.ces.clemson.edu>
2018-09-21 12:23:45 -0400

    add datacontainer for loading data and accessing easily

--- data-scripts/DataContainer.py
+++ data-scripts/DataContainer.py
@@ -18,12 +18,14 @@ class data_t(object):
 		n_idx = index * batch_size + batch_size
 		return self.data[idx:n_idx, :], self.labels[idx:n_idx, :]
 
+# expects a numpy array of data and a corresponding numpy array of labels
+# samples on the rows, features on the columns
 class DataContainer:
-	def __init__(self, data, total_gene_list=None):
-		self.num_classes = len(data)
-		self.label_names_ordered = []
+	def __init__(self, data, labels, train_split=0.8, test_split=0.2):
+		assert(data.shape[0] == labels.shape[0])
+		self.num_classes = labels.shape[1]
 		self.class_counts = {}
-		self.train, self.test = self.split_set(data, total_gene_list, sub_gene_list, train_split, test_split)
+		self.train, self.test = self.partition(data, labels, train_split, test_split)
 
 
 	#
@@ -36,9 +38,60 @@ class DataContainer:
 		self.train.labels = np.squeeze(self.train.labels[idxs])
 
 
+	#
+	# USAGE:
+	#	shuffle the data and labels in the same order for a data set and transform the data and labels into numpy arrays
+	# PARAMS:
+	#	data:	the data values for a dataset
+	# 	labels: the labels associated with data for a dataset
+	#
+	def shuffle_and_transform(self, data, labels):
+		stacked_d = np.vstack(data)
+		stacked_l = np.vstack(labels)
+
+		samples = random.sample(xrange(stacked_d.shape[0]),stacked_d.shape[0])
+
+		# convert lists to numpy arrays
+		stacked_d = stacked_d[samples]
+		stacked_l = stacked_l[samples]
+
+		return data_t(stacked_d,stacked_l)
+
 
 	#
 	# USAGE:
 	#       partition dataset into train/test sets
 	#
-	def partition(self, data, percent_samples=1.0, train_split=70, test_split=30):
+	def partition(self, data, labels, train_split=0.8, test_split=0.2):
+		x_train = []
+		y_train = []
+		x_test = []
+		y_test = []
+
+		for i in range(self.num_classes):
+			# find where the labels are equal to the certain class
+			idxs = np.where(np.argmax(labels, axis=1) == i)[0]
+			np.random.shuffle(idxs)
+
+			# record the class count information
+			self.class_counts[str(i)] = idxs.shape[0]
+
+			# get the int that splits the train/test setes
+			split = int(train_split * idxs.shape[0])
+
+			# append class data to respective lists
+			x_train.append(data[idxs[:split]])
+			y_train.append(labels[idxs[:split]])
+
+			x_test.append(data[idxs[split:]])
+			y_test.append(labels[idxs[split:]])
+
+		# format into datacontainer 
+		train = self.shuffle_and_transform(x_train, y_train)
+		test = self.shuffle_and_transform(x_test, y_test)
+
+		return [train, test]
+
+
+
+
